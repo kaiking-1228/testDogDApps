e78556d401ba303908406e34a86f062abdfc9c3d
kaiking-1228 <2050085041@qq.com>
2026-01-15 20:04:54 +0800

    16

--- assets/all-wallets-s4be0xZB.js
+++
@@ -1,6 +0,0 @@
-import{m as a}from"./core-DV59Jarr.js";import"./index-Co6Fu6Lk.js";import"./index.es-CnYZm0mG.js";const i=a`<svg fill="none" viewBox="0 0 24 24">
-  <path
-    style="fill: var(--wui-color-accent-100);"
-    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
-  />
-</svg>`;export{i as allWalletsSvg};
