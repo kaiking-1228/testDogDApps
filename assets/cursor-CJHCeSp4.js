12c86706bea705fe9f73c7adc6060e103369eb89
kaiking-1228 <2050085041@qq.com>
2026-01-22 20:03:03 +0800

    17

--- assets/cursor-CJHCeSp4.js
+++
@@ -1,3 +0,0 @@
-import{m as o}from"./core-ByKLjt9A.js";import"./index-Dgw2i2ZA.js";import"./index.es-CSeug3Vb.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
